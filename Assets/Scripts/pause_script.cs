4971adf35c17750e4118efda31d2af25575c3d19
godoy159 <godoyf@email.sc.edu>
2018-12-05 21:08:57 -0500

    commit
    commit

---
+++ Assets/Scripts/pause_script.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+//using static UnityEditor.CameraEditor;
+
+public class pause_script : MonoBehaviour
+{
+
+    public Transform Canvas;
+    public Transform applyImg;
+    public Transform Canvas_Set;
+
+    //private bool trackpadPressedDown;
+    //private bool trackpadPressedUp;
+    private void Start()
+    {
+
+        Canvas.gameObject.SetActive(false);
+        Canvas_Set.gameObject.SetActive(false);
+        applyImg.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            pause_function();
+        }
+
+    }
+    public void pause_function()
+    {
+        if (Canvas.gameObject.activeInHierarchy == false)
+        {
+
+            Canvas.gameObject.SetActive(true);
+            Canvas_Set.gameObject.SetActive(false);
+
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Canvas.gameObject.SetActive(false);
+            Canvas_Set.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void setting_function()
+    {
+        if (Canvas_Set.gameObject.activeInHierarchy == false)
+        {
+            Canvas_Set.gameObject.SetActive(true);
+            Canvas.gameObject.SetActive(false);
+            applyImg.gameObject.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Canvas_Set.gameObject.SetActive(true);
+            Canvas.gameObject.SetActive(false);
+
+        }
+
+    }
+    public void exitScene_Function()
+    {
+
+    }
+    public void ba_bu_func()
+    {
+        if (Canvas.gameObject.activeInHierarchy == false)
+        {
+            Canvas_Set.gameObject.SetActive(false);
+            Canvas.gameObject.SetActive(true);
+
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Canvas.gameObject.SetActive(false);
+            Canvas_Set.gameObject.SetActive(true);
+
+        }
+    }
+    public void apply_func()
+    {
+        if (applyImg.gameObject.activeInHierarchy == false)
+        {
+            applyImg.gameObject.SetActive(true);
+            //Canvas_Set.gameObject.SetActive(false);
+        }
+        else
+            Canvas_Set.gameObject.SetActive(false);
+    }
+
+}
