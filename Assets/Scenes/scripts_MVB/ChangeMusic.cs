852db18cb3635c0382556aec1adab388f382518d
Sadad <srkhan@email.sc.edu>
2018-12-06 19:56:49 -0500

    Updated scenes music on main menu
    Co-Authored-By: asanamandra <asanamandra@users.noreply.github.com>
    Co-Authored-By: godoy159 <godoy159@users.noreply.github.com>
    Co-Authored-By: Andy Michels <amichels1@users.noreply.github.com>
    Co-Authored-By: Kenneth Richardson <kennethr95@users.noreply.github.com>

---
+++ Assets/Scenes/scripts_MVB/ChangeMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMusic : MonoBehaviour {
+    public Slider volume;
+    public AudioSource myMusic;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        myMusic.volume = volume.value;
+	}
+}
