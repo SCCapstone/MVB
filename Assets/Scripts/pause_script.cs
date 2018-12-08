59f5126a47c6f40435ddd1d867edb13a7d58244b
Andy Michels <amichels@email.sc.edu>
2018-12-07 22:57:31 -0500

    POC-Complete
    Co-Authored-By: Andy Michels <amichels1@users.noreply.github.com>
    Co-Authored-By: asanamandra <asanamandra@users.noreply.github.com>
    Co-Authored-By: godoy159 <godoy159@users.noreply.github.com>
    Co-Authored-By: Sadad Khan <srkhan@users.noreply.github.com>
    Co-Authored-By: Kenneth Richardson <kennethr95@users.noreply.github.com>

---
+++ Assets/Scripts/pause_script.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//using static UnityEditor.CameraEditor;
+
+public class pause_script : MonoBehaviour {
+
+    public Transform PCanvas;
+  
+
+    private void Start()
+    {
+
+        PCanvas.gameObject.SetActive(false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        OVRInput.Update();
+  
+    }
+    public void pause_function()
+    {
+        if (PCanvas.gameObject.activeInHierarchy == false)
+        {
+
+            PCanvas.gameObject.SetActive(true);
+            //Canvas_Set.gameObject.SetActive(false);
+
+            Time.timeScale = 0;
+        }
+        else
+        {
+           PCanvas.gameObject.SetActive(false);
+            //Canvas_Set.gameObject.SetActive(false);
+            //Time.timeScale = 1;
+        }
+    }
+    public void res_function()
+    {
+        PCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void ba_bu_func()
+    {
+        if (PCanvas.gameObject.activeInHierarchy == false)
+        {
+            //Canvas_Set.gameObject.SetActive(false);
+            PCanvas.gameObject.SetActive(true);
+
+            Time.timeScale = 0;
+        }
+        else
+        {
+            PCanvas.gameObject.SetActive(false);
+            //Canvas_Set.gameObject.SetActive(true);
+
+        }
+    }
+    
+}
