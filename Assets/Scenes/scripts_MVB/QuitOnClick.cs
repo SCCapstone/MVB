a1471c210521e3858fe65c16ffffa3ab8277460f
Sadad <srkhan@email.sc.edu>
2019-03-22 14:29:22 -0400

    Fixed Main Menu Crashing bug
    wrote a different quit script,  so users can quit form the main menu, or a scene into the oculus go OS without it having to crash when restarting the app.
    
    Co-Authored-By: asanamandra <asanamandra@users.noreply.github.com>

--- Assets/Scenes/scripts_MVB/QuitOnClick.cs
+++ Assets/Scenes/scripts_MVB/QuitOnClick.cs
@@ -5,11 +5,14 @@ public class QuitOnClick : MonoBehaviour {
 
     public void Quit()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
+        //Quits Application without checking to see if if Unity Editor. Unity Editor will not be used in Oculus Go anyway. 
+        //This was the root cause of the menu crash.
         Application.Quit();
-#endif
+        //#if UNITY_EDITOR
+        //  UnityEditor.EditorApplication.isPlaying = false;
+        //#else
+
+        //#endif
     }
 }
 
