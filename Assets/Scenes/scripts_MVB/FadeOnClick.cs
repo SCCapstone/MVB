9094d4a4256a148529771600ceffc7186a4733df
godoy159 <godoyf@email.sc.edu>
2019-04-11 13:18:35 -0700

    fade_on_click_tans
    attempt 1-- failed

---
+++ Assets/Scenes/scripts_MVB/FadeOnClick.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public variable of type imagge
+    public Image fading_image;
+    public AnimationCurve curve;
+
+    void tart()
+    {
+        
+        StartCoroutine(FadeIn());
+    }
+    public void TrasitionFade(int scene)
+    {
+        StartCoroutine(fadeOut(scene));
+    }
+    //two functions.fading in and fading out
+
+    IEnumerator FadeIn()
+    {
+        float time = 0f;
+
+        while (time > 1f)
+        {
+            time += Time.deltaTime; //this will wait a small amount of time
+            float alphaValue = curve.Evaluate(time);
+            fading_image.color = new Color(0f, 0f, 0f, alphaValue);
+            yield return 0; //this will skip to the next frame
+        }
+        
+
+        //then it will load a scene
+    }
+  
+
+  
+
+
+    IEnumerator fadeOut(int scene)
+    {
+        float time = 1f;
+
+        while (time > 0f)
+        {
+            time -= Time.deltaTime; //this will wait a small amount of time
+            float alphaValue = curve.Evaluate(time);
+            fading_image.color = new Color(0f, 0f, 0f,alphaValue);
+            yield return 0; //this will skip to the next frame
+        }
+        SceneManager.LoadScene(scene);
+        
+        //then it will load a scene
+        
+    
+
+    }
+
+
+}
+
+//on the loadscreen on click script try this
+
+ //public FadeOnclick fadeTransition
+
+ //in the loadbyindex function
+ //fadeTransition.fadeTo(sceneIndex)
+
+ //on the quit function we can also do a debug statement
+ //Debug.Log("Thank you for coming")
+
+
+
--- Assets/Scenes/scripts_MVB/LoadScreenOnClick.cs
+++ Assets/Scenes/scripts_MVB/LoadScreenOnClick.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class LoadScreenOnClick : MonoBehaviour
 {
-
+    //public FadeOnClick fading;
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
