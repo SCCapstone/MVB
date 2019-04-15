1eb2bfa62d53cca2e8c9124e7d288f366ff477a0
Andy Michels <amichels@email.sc.edu>
2019-04-15 17:22:02 -0400

    Added Clemson Paws / functionality for RC1
    WIP Clemson Paw Interactivity without 360 videos for use with testing the RC1

---
+++ Assets/Scenes/scripts_MVB/CollectClemPaws.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectClemPaws : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    public void Collect()//(Collider other)
+    {
+        collectSound.Play();
+        ScoreSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+}
